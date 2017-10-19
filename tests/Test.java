7936531ac3f780e4af2d3f4e2b9673effad5b8bf
puzanov <puzanov@gmail.com>
2017-10-19 16:47:32 +0600

    simple check of transaction creation

--- tests/Test.java
+++ tests/Test.java
@@ -2,6 +2,12 @@ import junit.framework.TestCase;
 
 public class Test extends TestCase {
     public void testTransaction() {
-        assertTrue(true);
+        Transaction tx1 = new Transaction();
+        byte[] txID1 = tx1.getRawTx();
+        System.out.println("txID: " + txID1.toString());
+
+        Transaction tx2 = new Transaction();
+        byte[] txID2 = tx2.getRawTx();
+        System.out.println("txID: " + txID2.toString());
     }
 }
