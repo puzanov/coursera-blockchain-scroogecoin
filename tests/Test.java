71d4986fe84b3ceee7c797bc67b0b20d12a8408c
puzanov <puzanov@gmail.com>
2017-10-19 17:04:33 +0600

    failing test with transaction hash code

--- tests/Test.java
+++ tests/Test.java
@@ -1,7 +1,7 @@
 import junit.framework.TestCase;
 
 public class Test extends TestCase {
-    public void testTransaction() {
+    public void testTransactionIDs() {
         Transaction tx1 = new Transaction();
         byte[] txID1 = tx1.getRawTx();
         System.out.println("txID: " + txID1.toString());
@@ -9,5 +9,10 @@ public class Test extends TestCase {
         Transaction tx2 = new Transaction();
         byte[] txID2 = tx2.getRawTx();
         System.out.println("txID: " + txID2.toString());
+    }
+
+    public void testTransactionHashes() {
+        Transaction tx1 = new Transaction();
+        assertNotNull(tx1.getHash());
     }
 }
