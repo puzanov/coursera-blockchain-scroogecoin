371f5ffc7fa8395426ebc9218bb7a90342366eb3
puzanov <puzanov@gmail.com>
2017-10-23 20:33:33 +0600

    more transactions

--- tests/TestTransactions.java
+++ tests/TestTransactions.java
@@ -23,13 +23,24 @@ public class TestTransactions extends TestCase {
         KeyPair scroogeKeys = CryptoHelper.generateKeys();
         Transaction txCreateNewCoins = new Transaction();
         txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
 
         KeyPair goofyKeys = CryptoHelper.generateKeys();
         Transaction txPayOneCoinToGoofy = new Transaction();
-        txPayOneCoinToGoofy.addInput(txCreateNewCoins.getHash(), 0);
+        txPayOneCoinToGoofy.addInput(txCreateNewCoins.getHash(), 0); // first scrooge coin
         txPayOneCoinToGoofy.addOutput(1, goofyKeys.getPublic());
         txPayOneCoinToGoofy.addSignature(CryptoHelper.sign(scroogeKeys.getPrivate(), txPayOneCoinToGoofy.getRawDataToSign(0)), 0);
 
         assertTrue(Crypto.verifySignature(scroogeKeys.getPublic(), txPayOneCoinToGoofy.getRawDataToSign(0), txPayOneCoinToGoofy.getInput(0).signature));
+
+        KeyPair mikeyKeys = CryptoHelper.generateKeys();
+        Transaction txPayTwoCoinsToMikey = new Transaction();
+        txPayTwoCoinsToMikey.addInput(txCreateNewCoins.getHash(), 0); // second scrooge coin
+        txPayTwoCoinsToMikey.addInput(txPayOneCoinToGoofy.getHash(), 0); // fist goofy coin
+        txPayTwoCoinsToMikey.addOutput(1, mikeyKeys.getPublic());
+        txPayTwoCoinsToMikey.addOutput(1, mikeyKeys.getPublic());
+        txPayTwoCoinsToMikey.addSignature(CryptoHelper.sign(scroogeKeys.getPrivate(), txPayTwoCoinsToMikey.getRawDataToSign(0)), 0); // scrooge signs
+        txPayTwoCoinsToMikey.addSignature(CryptoHelper.sign(goofyKeys.getPrivate(), txPayTwoCoinsToMikey.getRawDataToSign(1)), 1); // goofy signs
     }
 }
