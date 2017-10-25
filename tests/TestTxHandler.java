408327d9f894d46c418b92d5376baad963628c41
puzanov <puzanov@gmail.com>
2017-10-25 10:30:19 +0600

    added test with wrong signature

--- tests/TestTxHandler.java
+++ tests/TestTxHandler.java
@@ -65,6 +65,45 @@ public class TestTxHandler extends TestCase {
 
         assertTrue(handler.isValidTx(txPayOneCoinToGoofy));
     }
+
+    public void testIsValidTx_WrongSign() {
+        KeyPair scroogeKeys = CryptoHelper.generateKeys();
+        Transaction txCreateNewCoins = new Transaction();
+        txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins.finalize();
+        byte[] txCreateNewCoinsHash = txCreateNewCoins.getHash();
+
+
+        Transaction txCreateNewCoins1 = new Transaction();
+        txCreateNewCoins1.addOutput(1, scroogeKeys.getPublic());
+        txCreateNewCoins1.finalize();
+        byte[] txCreateNewCoinsHash1 = txCreateNewCoins1.getHash();
+
+        UTXO utxo1 = new UTXO(txCreateNewCoinsHash, 0);
+        UTXO utxo2 = new UTXO(txCreateNewCoinsHash, 1);
+        UTXO utxo3 = new UTXO(txCreateNewCoinsHash1, 2);
+
+        assertTrue(Arrays.equals(utxo1.getTxHash(), txCreateNewCoinsHash));
+        assertTrue(Arrays.equals(utxo2.getTxHash(), txCreateNewCoinsHash));
+        assertTrue(Arrays.equals(utxo3.getTxHash(), txCreateNewCoinsHash1));
+
+        UTXOPool utxoPool = new UTXOPool();
+        utxoPool.addUTXO(utxo1, txCreateNewCoins.getOutput(0));
+        utxoPool.addUTXO(utxo2, txCreateNewCoins.getOutput(1));
+        utxoPool.addUTXO(utxo3, txCreateNewCoins.getOutput(2));
+
+        KeyPair goofyKeys = CryptoHelper.generateKeys();
+        Transaction txPayOneCoinToGoofy = new Transaction();
+        txPayOneCoinToGoofy.addInput(txCreateNewCoinsHash, 0); // first scrooge coin
+        txPayOneCoinToGoofy.addOutput(1, goofyKeys.getPublic());
+        txPayOneCoinToGoofy.addSignature(CryptoHelper.sign(goofyKeys.getPrivate(), txPayOneCoinToGoofy.getRawDataToSign(0)), 0);
+
+        TxHandler handler = new TxHandler(utxoPool);
+
+        assertFalse(handler.isValidTx(txPayOneCoinToGoofy));
+    }
 }
 
 
